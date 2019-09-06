bde90f4c47260acdb4594438e2e1028d66d4a1d7
DESKTOP-9BH3I8E\hjkim <hjkim@gaia3d.com>
2019-09-06 16:28:51 +0900

    changed ASSIMP library into the latest version

--- F4DConverter/converter/reader/ClassicFormatReader.h
+++ F4DConverter/converter/reader/ClassicFormatReader.h
@@ -2,19 +2,7 @@
 
 #if defined(CLASSICFORMAT)
 
-//#define ASSIMP_VERSION_3_1_1
-#define ASSIMP_VERSION_3_2
-
-#if defined(ASSIMP_VERSION_3_2)
-#ifdef _DEBUG
-#pragma comment(lib, "../external/assimp-3.2/lib/assimp-vc100-mtd.lib")
-#else
-#pragma comment(lib, "../external/assimp-3.2/lib/assimp-vc100-mt.lib")
-#endif
-#elif defined(ASSIMP_VERSION_3_1_1)
-#pragma comment(lib, "../external/assimp-3.1.1-win-binaries/lib64/assimp.lib")
-#endif
-
+#pragma comment(lib, "../external/assimp-4.1/lib/x64/assimp-vc140-mt.lib")
 
 #include "aReader.h"
 
